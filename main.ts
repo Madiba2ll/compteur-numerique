e5b88a3adeba21c97dd80cb9523dd5138476f31f
Madiba2ll <116099894+Madiba2ll@users.noreply.github.com>
2022-10-24 15:15:40 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-let nb_personne = 0
 input.onButtonPressed(Button.A, function () {
     if (nb_personne >= 10) {
         basic.showNumber(10)
@@ -6,6 +5,10 @@ input.onButtonPressed(Button.A, function () {
         nb_personne = nb_personne + 1
         basic.showNumber(nb_personne)
     }
+})
+input.onButtonPressed(Button.AB, function () {
+    nb_personne = 0
+    basic.showNumber(nb_personne)
 })
 input.onButtonPressed(Button.B, function () {
     if (nb_personne <= 0) {
@@ -15,15 +18,11 @@ input.onButtonPressed(Button.B, function () {
         basic.showNumber(nb_personne)
     }
 })
+let nb_personne = 0
+basic.showNumber(nb_personne)
+nb_personne = 0
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.AB)) {
-        nb_personne = 0
-    }
-    while (nb_personne <= 0) {
-        basic.showNumber(0)
-    }
     while (nb_personne >= 10) {
-        basic.showNumber(nb_personne)
         basic.showLeds(`
             . # # # .
             . # # # .
@@ -31,11 +30,12 @@ basic.forever(function () {
             . . . . .
             . # # # .
             `)
-    }
-    if (nb_personne >= 10) {
-        nb_personne = 10
-    }
-    if (nb_personne <= 0) {
-        nb_personne = 0
+        basic.showLeds(`
+            # . # # #
+            # . # . #
+            # . # . #
+            # . # . #
+            # . # # #
+            `)
     }
 })
